a621b8cd8cd48cf9900bb2ae83676dc3bc198df6
CS Yang <148856699+rbmakers@users.noreply.github.com>
2024-01-03 13:52:41 +0800

    Update self_balance_test.py

--- self_balance_test.py
+++ self_balance_test.py
@@ -1,23 +1,18 @@
-from machine import Pin, I2C
 import machine
 import time
 from pyb import Timer
 from micropython import const
+import time
+
 from bmp280 import BMP280
 from lsm9ds1 import LSM9DS1
 
 BMP280_CASE_INDOOR = const(5)
-
-tim4 = Timer(4, freq=1000)
-tim5 = Timer(5, freq=1000)
-
-bus = I2C(2)
-lsm = LSM9DS1(bus)
-
-i2c = machine.I2C(scl=machine.Pin("P7"), sda=machine.Pin("P8"))
-bmp = BMP280(i2c)
+bus1 = machine.I2C(scl=machine.Pin("P7"), sda=machine.Pin("P8"))
+bmp = BMP280(bus1)
 bmp.use_case(BMP280_CASE_INDOOR)
-
+bus2 = machine.I2C(scl=machine.Pin("P4"), sda=machine.Pin("P5"))
+lsm = LSM9DS1(bus2)
 
 PWM1 = 0
 PWM2 = 0
@@ -54,14 +49,13 @@ def pd(s):
 while True:
 
     p_mmHg=bmp.pressure
-    pressure=p_mmHg/133.3224
+    pressure=round(p_mmHg/133.32, 1)
 
     acc_z = int(1000*lsm.read_accel()[2])
     acc_y = int(1000*lsm.read_accel()[1])
     acc_x = int(1000*lsm.read_accel()[0])
-    print(pressure, acc_z, acc_y, acc_x, UPP, PWM1, PWM2, PWM3, PWM4)
 
-    if UPP == 1 :  # Take-Off
+    if UPP == 1 :
         if acc_z > 970:
             PWM1 = round(PWM1, 2) + STEP
             PWM2 = round(PWM2, 2) + STEP
@@ -89,7 +83,7 @@ while True:
 
         if pressure < 760.6 or PWM1 > 45 or PWM2 > 45 or PWM3 > 45 or PWM4 > 45 : UPP = 0
 
-    elif UPP == 0 :  # Landing
+    elif UPP == 0 :
         if acc_z > 970:
             PWM1 = round(PWM1, 2) - STEP
             PWM2 = round(PWM2, 2) - STEP
@@ -118,9 +112,6 @@ while True:
         if PWM1 < 5 or PWM2 < 5 or PWM3 < 5 or PWM4 < 5 : break
 
 
-    #ch1 = tim5.channel(3, Timer.PWM, pin=Pin("M1"), pulse_width_percent=PWM1)
-    #ch2 = tim5.channel(4, Timer.PWM, pin=Pin("M2"), pulse_width_percent=PWM2)
-    #ch3 = tim4.channel(3, Timer.PWM, pin=Pin("M3"), pulse_width_percent=PWM3)
-    #ch4 = tim4.channel(4, Timer.PWM, pin=Pin("M4"), pulse_width_percent=PWM4)
+    print(UPP, pressure, acc_z, acc_y, acc_x, PWM1, PWM2, PWM3, PWM4)
 
-    time.sleep_ms(500)
+    time.sleep_ms(200)
